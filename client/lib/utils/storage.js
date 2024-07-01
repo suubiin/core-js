9e4be325a45a2f2143c2d3b12cc8323b0d6d5b17
Suubiin <suubiin0819@gmail.com>
2024-07-01 11:49:43 +0900

    storage

--- client/lib/utils/storage.js
+++ client/lib/utils/storage.js
@@ -2,22 +2,9 @@ import { isString } from './type.js'
 
 
 
-console.log('storage');
-
 const {localStorage:storage} = window;
 
-const user = {
-  name:'tiger',
-  age:40,
-  gender: 'male',
-  email:'tiger@naver.com'
-}
-
-// localStorage.setItem('user',JSON.stringify(user))
-
-
-function setStorage(key,value){
-  
+export function setStorage(key,value){
   return new Promise((resolve, reject) => {
     if(isString(key)){
       storage.setItem(key,JSON.stringify(value));
@@ -28,9 +15,7 @@ function setStorage(key,value){
   })
 }
 
-
-function getStorage(key){
-
+export function getStorage(key){
   return new Promise((resolve, reject) => {
     if(isString(key))  {
       resolve(JSON.parse(storage.getItem(key)))
@@ -40,6 +25,11 @@ function getStorage(key){
   })
 }
 
+export function deleteStorage(key){
 
+  return new Promise((resolve, reject) => {
+    !key ? storage.clear() : storage.removeItem(key);
+    resolve()
+  })
 
-// getStorage('user')
+}
--- client/main.js
+++ client/main.js
@@ -1,1 +1,23 @@
-import {  } from "./lib/index.js";
+import { clearContents, deleteStorage, getNode, getStorage, setStorage } from "./lib/index.js";
+
+const textField = getNode('#textField');
+const clear = getNode('button[data-name="clear"]')
+
+getStorage('text')
+.then((res)=>{
+  textField.value = res;
+})
+
+function handleTextField(){
+  const value = this.value;
+
+  setStorage('text',value);
+}
+
+function handleClear(){
+  deleteStorage('text');
+  clearContents(textField);
+}
+
+textField.addEventListener('input', handleTextField);
+clear.addEventListener('click', handleClear);
