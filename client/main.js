96dce74b503ba23a2f20e87f336d750e8d3289ac
Suubiin <suubiin0819@gmail.com>
2024-07-03 13:45:38 +0900

    learn webcomponent
    -event binding

--- client/main.js
+++ client/main.js
@@ -3,19 +3,18 @@ class UserCard extends HTMLElement{
     super();
     this.attachShadow({mode:'open'});
     this.shadowRoot.innerHTML = `
-      <style>
-        :host{
-          background-color : orange;
-        }
-      </style>
-        <div> nickName : kind-tiger </div>
-        <slot name="username"></slot>
-        <slot name="age"></slot>
-        <slot name="gender"></slot>
-        <slot name="common"></slot>
-        <slot></slot>
-        <slot name="email">any@naver.com</slot>
+      <button type="button">btn</button>
     `
+
+    this.button = this.shadowRoot.querySelector('button');
+  }
+
+  connectedCallback(){
+    this.button.addEventListener('click',()=>this.clickMe())
+  }
+
+  clickMe(){
+    console.log(this);
   }
 }
 
