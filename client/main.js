6b88f475054d3ca10e6cda03bb7f716ac41e88a2
Suubiin <suubiin0819@gmail.com>
2024-07-03 10:28:34 +0900

    learn webcomponent
    -observedAttributes
    -attributechanged

--- client/main.js
+++ client/main.js
@@ -1,21 +1,37 @@
-class MyElement extends HTMLElement{
-  constructor(){
+class Button extends HTMLElement {
+  constructor() {
     super();
+
+    this.button = document.querySelector('button');
   }
 
-  connectedCallback(){
-    console.log('탄생함');
+  connectedCallback() {
+    this._render();
   }
-  
-  disconnectedCallback(){
-    console.log('죽음');
+
+  disconnectedCallback() {}
+
+  static get observedAttributes() {
+    return ['id'];
+  }
+
+  attributeChangedCallback(name, oldValue, newValue) {
+    if (oldValue !== newValue) {
+      this._render();
+    }
+  }
+
+  _render() {
+    this.button.textContent = this.id;
   }
 }
 
+customElements.define('c-button', Button);
 
-customElements.define('c-element', MyElement)
+const c = document.querySelector('c-button');
 
-const elem = document.createElement('c-element');
-const app = document.getElementById('app');
+let count = 0;
 
-app.appendChild(elem);
+c.addEventListener('click', () => {
+  c.setAttribute('id', ++count);
+});
