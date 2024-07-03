94be0fb55969ad19ad34b9bf0a71c46d21f2d691
Suubiin <suubiin0819@gmail.com>
2024-07-03 13:27:14 +0900

    learn webcomponent
    -DOM style

--- client/main.js
+++ client/main.js
@@ -1,16 +1,22 @@
-const template = document.createElement('template');
+class UserCard extends HTMLElement{
+  constructor(){
+    super();
+    this.attachShadow({mode:'open'});
+    this.shadowRoot.innerHTML = `
+      <style>
+        :host{
+          background-color : orange;
+        }
+      </style>
+        <div> nickName : kind-tiger </div>
+        <slot name="username"></slot>
+        <slot name="age"></slot>
+        <slot name="gender"></slot>
+        <slot name="common"></slot>
+        <slot></slot>
+        <slot name="email">any@naver.com</slot>
+    `
+  }
+}
 
-template.innerHTML = `
-  <div>bye</div>
-  <div>javascript</div>
-`
-
-console.log(template);
-
-
-const app = document.querySelector('#app');
-const temp = document.querySelector('#temp');
-
-const clone = temp.content.cloneNode(true)
-
-app.appendChild(clone)
+customElements.define('user-card',UserCard);
