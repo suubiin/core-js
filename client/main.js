91c64c4577c16c11f01fbbd805047ab0a2559079
Suubiin <suubiin0819@gmail.com>
2024-07-03 11:16:13 +0900

    learn webcomponent
    -shadow DOM

--- client/main.js
+++ client/main.js
@@ -1,7 +1,15 @@
-class Button extends HTMLButtonElement {
+class Button extends HTMLElement {
 
   constructor(){
     super();
+
+    // c-button의 쉐도우 돔을 열어줘
+    this.attachShadow({mode:'open'});
+
+    // 그리고 그 안에 내가 원하는 태그 집어 넣을거야
+    this.shadowRoot.innerHTML = `
+      <button>hello</button>
+    `
   }
 
   connectedCallback(){
@@ -16,4 +24,4 @@ class Button extends HTMLButtonElement {
 }
 
 
-customElements.define('c-button',Button,{extends:'button'});
+customElements.define('c-button',Button);
